c733724219cca1354a0eec83d563a591b53286a6
Pedro Reis <pedro_reis14@hotmail.com>
2019-02-07 10:23:23 -0200

    adding swagger generator

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import jwt from 'express-jwt'
 import http from 'http'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
-
+// swagger
+import expressSwagger from 'express-swagger-generator'
 // routes
 import projectRouter from './src/routes/projects'
 import userRouter from './src/routes/user'
 
 const app = express()
+const swagger = expressSwagger(app)
 
 // loger
 app.use(morgan('dev'))
@@ -40,7 +42,35 @@ app.use('/login', userRouter)
 
 const server = http.createServer(app)
 
+const options = {
+	swaggerDefinition: {
+		info: {
+			description: 'This is a sample server',
+			title: 'Swagger',
+			version: '1.0.0',
+		},
+		host: 'localhost:3000',
+		basePath: '/',
+		produces: [
+			'application/json',
+			'application/xml'
+		],
+		schemes: ['http'],
+		securityDefinitions: {
+			JWT: {
+				type: 'apiKey',
+				in: 'header',
+				name: 'Authorization',
+				description: '',
+			}
+		}
+	},
+	basedir: __dirname, //app absolute path
+	files: ['./routes/**/*.js'] //Path to the API handle folder
+}
+
 const port = process.env.PORT || 4000
+swagger(options)
 server.listen(port, () => {
 	console.log(`• Server started succesfully, running at port ${port} 🚀`)
 })
