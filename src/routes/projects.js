a3c0bed6577148262955355599e808769c6a3db9
Pedro Reis <pedro_reis14@hotmail.com>
2019-02-19 10:00:34 -0300

    changing issues response

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -6,26 +6,28 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
 	if (!req.user || !req.user.apiKey) res.sendStatus(401)
+	else {
+		const { data } = await getUserProjects(req.user.apiKey)
 
-	const { data } = await getUserProjects(req.user.apiKey)
+		const projects = data.projects.map(project => (
+			{ nome: project.name, identificador: project.identifier }
+		))
 
-	const projects = data.projects.map(project => (
-		{ nome: project.name, identificador: project.identifier }
-	))
-
-	res.status(200).send({ projects })
+		res.status(200).send({ projects })
+	}
 })
 
 router.get('/:project/issues', async (req, res) => {
 	if (!req.user || !req.user.apiKey) res.sendStatus(401)
-
-	const { data } = await getIssuesList(req.user.apiKey, req.params.project)
-	const issuesp = data.issues.map(async issue => {
-		const { data } = await getIssueDetail(req.user.apiKey, issue.id)
-		return { issue: data.issue }
-	})
-	const issues = await Promise.all(issuesp).then(i => i)
-	res.send({ issues })
+	else {
+		const { data } = await getIssuesList(req.user.apiKey, req.params.project)
+		const issuesp = data.issues.map(async issue => {
+			const { data } = await getIssueDetail(req.user.apiKey, issue.id)
+			return data.issue
+		})
+		const issues = await Promise.all(issuesp).then(i => i)
+		res.send({ issues })
+	}
 })
 
 export default router
